e9807ce59b167dc0582b6e92eab8e156cccdcef7
Mihály Egeresi <egeresi.mihaly2@gmail.com>
2022-08-29 11:27:37 +0200

    registration form update

--- main.py
+++ main.py
@@ -28,10 +28,14 @@ class RegistrationForm(QWidget):
         #connect signals/ slot mechanism
 
         save_button.clicked.connect(self.save_action)
+        self.first_name_field.textChanged.connect(self.first_name_changed)
 
     def save_action(self):
         print(f"First name: {self.first_name_field.text()}")
         print(f"Last name: {self.last_name_field.text()}")
+
+    def first_name_changed(self,value):
+        print(value)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
